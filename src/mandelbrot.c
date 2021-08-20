696d33987ca056353b756f2e1e79c1d87e1a5b4f
aakashm101 <aakashmanivannan02@gmail.com>
2021-08-20 23:30:19 +0530

    Redundant color palettes removed

--- src/mandelbrot.c
+++ src/mandelbrot.c
@@ -3,7 +3,7 @@
 #include "globals.h"
 #include "mandelbrot.h"
 
-const Uint32 PALETTE1[] =
+const Uint32 PALETTE[] =
 {
 	0x000000, 0xff3242, 0xff3a40, 0xff403d, 0xff473b, 0xff4d38, 0xff5335, 0xff5932,
 	0xff5f2f, 0xff652c, 0xff6b29, 0xff7026, 0xff7523, 0xff7b1f, 0xff801b, 0xff8517,
@@ -12,16 +12,7 @@ const Uint32 PALETTE1[] =
 	0xfcd903, 0xfade0c, 0xf8e215, 0xf6e71c, 0xf4eb23, 0xf1f029, 0xeff530, 0xecf936
 };
 
-const Uint32 PALETTE2[] = {
-	0x000000, 0x4400fa, 0x6000f4, 0x7400ee, 0x8500e8, 0x9300e2, 0xa000dc, 0xab00d5,
-	0xb500ce, 0xbf00c8, 0xc700c1, 0xcf00ba, 0xd600b3, 0xdc00ac, 0xe200a6, 0xe8009f,
-	0xed0098, 0xf10091, 0xf5008a, 0xf90084, 0xfc007d, 0xff0077, 0xff0070, 0xff006a,
-	0xff0064, 0xff005e, 0xff0058, 0xff0052, 0xff004c, 0xff0046, 0xff0040, 0xff003b,
-	0xff0035, 0xff002f, 0xff002a, 0xff0023, 0xff001d, 0xff0015, 0xff000c, 0xff0000
-};
-
-const int PALETTE1_SIZE = sizeof(PALETTE1) / sizeof(Uint32);
-const int PALETTE2_SIZE = sizeof(PALETTE2) / sizeof(Uint32);
+const int PALETTE_SIZE = sizeof(PALETTE) / sizeof(Uint32);
 
 void processMandelbrot(WindowParameters* const wp, const UserPreferences* const up) 
 {
@@ -58,8 +49,8 @@ void processMandelbrot(WindowParameters* const wp, const UserPreferences* const 
 			}
 			else 
 			{
-				iterations = iterations % PALETTE1_SIZE;
-				pixel = (PALETTE1[iterations] << 8) | ALPHA;
+				iterations = iterations % PALETTE_SIZE;
+				pixel = (PALETTE[iterations] << 8) | ALPHA;
 			}
 
 			// Invert the colors if the user chooses to invert colors
